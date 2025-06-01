6366b90b0ca377c364ef8aa254472504e33d26ca
Adeliaamd <adeliaamd02@gmail.com>
2025-06-01 13:32:52 +0700

    color

--- resources/views/dashboard/layout/main.blade.php
+++ resources/views/dashboard/layout/main.blade.php
@@ -27,11 +27,11 @@
             <footer>
                 <div class="footer clearfix mb-0 text-muted">
                     <div class="float-start">
-                        <p>2023 &copy; Mazer</p>
+                        <p>2025 &copy; Shabrina</p>
                     </div>
                     <div class="float-end">
                         <p>Crafted with <span class="text-danger"><i class="bi bi-heart-fill icon-mid"></i></span>
-                            by <a href="https://saugi.me">Saugi</a></p>
+                            by <a href="https://saugi.me">Kelompok 4</a></p>
                     </div>
                 </div>
             </footer>
